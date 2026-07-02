dbf1d2ee176ba4dcda27cbeecc7486c466998cec
Ashitha <ashithamary2204@gmail.com>
2026-07-02 19:08:27 +0530

    Day 1

---
+++ day-1/first-js/script.js
@@ -0,0 +1,33 @@
+console.log("Hello")
+document.write("hello")
+
+let name1="ASHITHA"
+console.log(name1)
+
+console.log(typeof(name1))
+
+let salary=80000.50
+console.log(typeof(salary))
+
+var invalid=false
+console.log(typeof(invalid))
+
+console.log(2+2)
+console.log(2*2)
+
+let num1=3
+let num2=4
+console.log(num1+num2)
+console.log(num1*num2)
+
+console.log(10 % 5)
+console.log(5 ** 2)
+
+let x=5
+x+=5
+console.log(x)
+
+let flag1=true
+let flag2=false
+console.log(flag1 == flag2)
+console.log(flag1 === flag2)
